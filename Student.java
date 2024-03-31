6656a321e64166a3422690d4ba3e5762335db456
Vaishali <01fe22bca211@kletech.ac.in>
2024-03-31 13:17:08 +0530

    student java code

---
+++ Student.java
@@ -0,0 +1,35 @@
+public class Student {
+
+    String name; // Removed the initialization here
+    
+    int age;
+    
+    String month;
+    
+    int date;
+    
+    int year;
+    
+    public Student(int date, String month, int year) {
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
+    
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+    
+    public void read() {
+        System.out.println("Date of birth of the Student is " + date + "/" + month + "/" + year);
+    }
+    
+    public static void main(String[] args) {
+        Student s = new Student(15, "Feb", 2005); // Corrected the syntax for creating a new Student object
+        s.name = "vaishali"; // Set the name separately
+        s.age = 19; // Set the age separately
+        s.display();
+        s.read();
+    }
+}
